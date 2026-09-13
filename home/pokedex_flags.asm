1a0a9fa2a736d8ae8cf3a033d1f7deb09b9d2d60
SakuraiTsubaki <nokang3@naver.com>
2026-09-13 19:35:04 +0900

    Reconstruct shared Bank 00 Pokédex flag helpers

---
+++ home/pokedex_flags.asm
@@ -0,0 +1,58 @@
+CountSetBits::
+	ld c, 0
+.next
+	ld a, [hli]
+	ld e, a
+	ld d, 8
+.count
+	srl e
+	ld a, 0
+	adc c
+	ld c, a
+	dec d
+	jr nz, .count
+	dec b
+	jr nz, .next
+	ld a, c
+	ld [wNumSetBits], a
+	ret
+
+GetWeekday::
+	ld a, [wCurDay]
+.mod
+	sub 7
+	jr nc, .mod
+	add 7
+	ret
+
+SetSeenAndCaughtMon::
+	push af
+	ld c, a
+	ld hl, wPokedexCaught
+	ld b, SET_FLAG
+	call PokedexFlagAction
+	pop af
+
+SetSeenMon::
+	ld c, a
+	ld hl, wPokedexSeen
+	ld b, SET_FLAG
+	jr PokedexFlagAction
+
+CheckCaughtMon::
+	ld c, a
+	ld hl, wPokedexCaught
+	ld b, CHECK_FLAG
+	jr PokedexFlagAction
+
+CheckSeenMon::
+	ld c, a
+	ld hl, wPokedexSeen
+	ld b, CHECK_FLAG
+
+PokedexFlagAction::
+	ld d, 0
+	predef SmallFarFlagAction
+	ld a, c
+	and a
+	ret
