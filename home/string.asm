8dc8e898c3d78ce1e752da143dce84bcb5e7dd79
SakuraiTsubaki <nokang3@naver.com>
2026-09-13 19:38:29 +0900

    Reconstruct regional Bank 00 string initialization

---
+++ home/string.asm
@@ -0,0 +1,51 @@
+InitString::
+	push hl
+	jr _InitString
+
+InitName::
+	push hl
+	ld c, NAME_LENGTH - 1
+
+_InitString::
+	push bc
+.loop
+	ld a, [hli]
+	cp '@'
+	jr z, .blank
+IF DEF(_KOREAN)
+	cp $c
+	jr nc, .single_byte
+	cp $b
+	jr nz, .notblank
+	dec c
+	jr z, .blank
+	ld a, [hli]
+	cp $ff
+	jr nz, .notblank
+	jr .next
+.single_byte
+	cp ' '
+	jr nz, .notblank
+.next
+	dec c
+	jr nz, .loop
+ELSE
+	cp ' '
+	jr nz, .notblank
+	dec c
+	jr nz, .loop
+ENDC
+.blank
+	pop bc
+	ld l, e
+	ld h, d
+	pop de
+	ld b, 0
+	inc c
+	call CopyBytes
+	ret
+
+.notblank
+	pop bc
+	pop hl
+	ret
