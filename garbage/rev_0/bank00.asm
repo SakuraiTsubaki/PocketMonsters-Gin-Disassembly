db4f77e6fb698286f3f0fe3db44869ceeec897cf
SakuraiTsubaki <nokang3@naver.com>
2026-09-13 19:50:14 +0900

    Add exact Japanese Silver Rev 0 Bank 00 garbage data

---
+++ garbage/rev_0/bank00.asm
@@ -0,0 +1,14 @@
+; Japanese Silver Rev 0 ROM0 trailing garbage.
+; Exact bytes at $3f0f-$3fbf. Rev A contains zero fill in this region.
+	db $d1,$e1,$c9,$fa,$75,$d6,$fe,$04,$28,$12,$fe,$08,$28,$0e,$fa,$65
+	db $d5,$cb,$57,$20,$0c,$a7,$c9,$11,$13,$00,$37,$c9,$11,$21,$00,$37
+	db $c9,$fa,$c6,$d9,$fe,$0a,$20,$ed,$fa,$c7,$d9,$fe,$0f,$28,$04,$fe
+	db $11,$20,$e2,$11,$58,$00,$37,$c9,$cd,$12,$3f,$d8,$cd,$03,$2e,$c9
+	db $3e,$20,$ea,$98,$c3,$ea,$9c,$c3,$3e,$50,$ea,$99,$c3,$3e,$58,$ea
+	db $9d,$c3,$af,$ea,$9b,$c3,$ea,$9f,$c3,$fa,$96,$c1,$fe,$64,$30,$16
+	db $c6,$01,$27,$47,$cb,$37,$e6,$0f,$c6,$f6,$ea,$9a,$c3,$78,$e6,$0f
+	db $c6,$f6,$ea,$9e,$c3,$c9,$3e,$ff,$ea,$9a,$c3,$ea,$9e,$c3,$c9,$fa
+	db $cc,$c0,$cb,$47,$20,$17,$fa,$fe,$c0,$cb,$47,$20,$10,$fa,$30,$c1
+	db $cb,$47,$20,$09,$fa,$62,$c1,$cb,$47,$20,$02,$a7,$c9,$37,$c9,$af
+	db $ea,$cc,$c0,$ea,$9c,$c1,$e0,$10,$e0,$11,$e0,$12,$e0,$13,$e0,$14
+	db $c9
